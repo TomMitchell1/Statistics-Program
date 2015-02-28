daf573f13561a891d3de8fd13ba4a548959a3bb5
TomMitchell1 <tom.mitchl@gmail.com>
2015-02-28 20:12:39 +1100

    Added in interface for the program and finalised the hypothesis testing. Tested the shearstrength file with the program and compared the results to the results obtained in matlab.

--- statPackage.c
+++ statPackage.c
@@ -1,3 +1,28 @@
+/*
+	Statistics Package by Tom Mitchell
+
+	This program takes uses a file location from the command line 
+	when the program is run. This file has to have 1 piece of data
+	per each line of the file.
+	
+	When the data is fully imported the program then prints out 
+	statistical data.
+	This includes:
+		-Mean
+		-Variance
+		-Standard deviation
+		-5 number summary
+		-Confidence intervals 99% and 95%
+		-Prediction intervals 99% and 95%
+	
+	The user will then be able to test hypotheses to see if claims
+	about the mean of the data should be rejected or accepted.
+	
+	Tests for mean,variance and standard deviation were done in test function.
+	The tests for the 5 number summary and confidence intervals were done by
+	checking the results against the results from matlab
+		
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -5,12 +30,21 @@
 #include <string.h>
 #include "tree.h"
 
+tree importData(char* string);
 void treeTests(void);
+void stats(tree t);
 
 int main( int argc, char* argv[]) {
-
-	treeTests();
-
+	tree t;
+	//treeTests();
+	if ( argc == 2 ){
+		t=importData(argv[1]);
+		stats(t);
+	} else {
+		printf("Did not give a file name.\n");
+	}
+	
+	
 	return EXIT_SUCCESS;
 }
 
@@ -140,4 +174,75 @@ void treeTests(void){
 	t=destroyTree(t);
 	assert(treeSize(t)==0);
 	assert(t==NULL);
+}
+
+tree importData(char* string){
+	tree t=newTree();
+	double data;
+	FILE *fp;
+	fp = fopen(string, "r+");
+	if(fp==NULL){
+	} else {
+		while(fscanf(fp,"%lf",&data) == 1)  {  
+        	t=insertNode(t,data);  
+		}  
+	}
+	return t;
+}
+
+void stats(tree t){
+	char string[3];
+	double hypothesis=0;
+	char hypothesisTestType='0';
+	if(t!=NULL){
+		//Print out the statistical information
+		printf("Number of data points is %d\n",treeSize(t));
+		printf("Sample mean = %.2f\n",findMean(t));
+		printf("Sample variance = %.2f\n",findVariance(t));
+		printf("Sample standard deviation = %.2f\n",findStdDeviation(t));
+		fiveNumberSummary(t);
+		confidenceInterval(t);
+		predictionInterval(t);
+		printf("Would you like to test hypotheses?\n");
+		printf("yes or no: "); 
+		scanf("%s",string);
+		while(strcmp (string,"yes") == 0){
+		
+			printf("Please enter a claimed average for hypothesis testing:");
+			scanf("%lf",&hypothesis);
+			printf("\nWhat sort of hypothesis test would you like to use?\n");
+			printf("Please type a,b,c for the type of hypothesis test:\n\n");
+			printf("  a)  Two-sided alternative test\n");
+			printf("  b)  One-sided alternative test: ");
+			printf("sample mean is not greater than hypothesis\n");
+			printf("  c)  One-sided alternative test: ");
+			printf("sample mean is not less than hypothesis\n\n");
+		
+			printf("Hypothesis test: ");
+			scanf(" %c",&hypothesisTestType);
+		
+			printf("\n");
+		
+			if(hypothesisTesting(t,hypothesisTestType,hypothesis)==TRUE){
+				printf("The hypothesis should be rejected\n");
+				printf(" at a significance level of 0.05.\n");
+				if(hypothesisTestType=='a'){
+					printf("Using a two-sided alternative test.\n");
+				} else {
+					printf("Using a one-sided alternative test.\n");
+				}
+			} else {
+				printf("The hypothesis should not be rejected");
+				printf(" at a significance level of 0.05.\n");
+				if(hypothesisTestType=='a'){
+					printf("Using a two-sided alternative test.\n");
+				} else {
+					printf("Using a one-sided alternative test.\n");
+				}
+			}
+			printf("\nWould you like to test hypotheses?\n");
+			printf("type yes or no: "); 
+			scanf("%s",string);
+		}	
+	}
 }
