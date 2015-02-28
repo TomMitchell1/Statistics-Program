f07d03d798e72f70c4e31d057fda81b0c1b29097
TomMitchell1 <tom.mitchl@gmail.com>
2015-02-28 15:39:29 +1100

    Added in a few more tests and command line functionality to test multiple hypotheses.

--- statPackage.c
+++ statPackage.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 #include "tree.h"
 
 void treeTests(void);
@@ -14,6 +15,7 @@ int main( int argc, char* argv[]) {
 }
 
 void treeTests(void){
+	char string[3];
 	double hypothesis=0;
 	char hypothesisTestType='0';
 	//Test tree construction
@@ -36,26 +38,22 @@ void treeTests(void){
 	assert(treeSize(t)==7);
 	showTree(t);
 	
-	
+	assert(t!=NULL);
 	assert(round(findMean(t) * 100) / 100==5.71);
-	//assert(findMedian(t)==5.5);
-	if(t!=NULL){
-		
-		//Print out the statistical information
-		printf("Number of data points is %d\n",treeSize(t));
-		printf("Sample mean = %.2f\n",findMean(t));
-		printf("Sample variance = %.2f\n",findVariance(t));
-		printf("Sample standard deviation = %.2f\n",findStdDeviation(t));
-		printf("Sample median = %.2f\n",findMedian(t));
-		fiveNumberSummary(t);
-		confidenceInterval(t);
-		predictionInterval(t);
-		
-		
-		
-		
-		
-	}
+	assert(findMedian(t)==6);
+	assert(round(findVariance(t) * 100) / 100==9.24);
+	assert(round(findStdDeviation(t) * 100) / 100==3.04);
+	
+	//Print out the statistical information
+	printf("Number of data points is %d\n",treeSize(t));
+	printf("Sample mean = %.2f\n",findMean(t));
+	printf("Sample variance = %.2f\n",findVariance(t));
+	printf("Sample standard deviation = %.2f\n",findStdDeviation(t));
+	printf("Sample median = %.2f\n",findMedian(t));
+	fiveNumberSummary(t);
+	confidenceInterval(t);
+	predictionInterval(t);	
+	
 	printf("\n\n\n");
 	t=insertNode(t,8);
 	assert(treeSize(t)==8);
@@ -77,8 +75,11 @@ void treeTests(void){
 	assert(treeSize(t)==15);
 	showTree(t);
 	
-	//assert(findMean(t)==5.25);
-	//assert(findMedian(t)==5.5);
+	assert(t!=NULL);
+	assert(round(findMean(t) * 100) / 100==6.2);
+	assert(findMedian(t)==7);
+	assert(round(findVariance(t) * 100) / 100==7.89);
+	assert(round(findStdDeviation(t) * 100) / 100==2.81);
 	if(t!=NULL){
 		
 		//Print out the statistical information
@@ -90,44 +91,47 @@ void treeTests(void){
 		fiveNumberSummary(t);
 		confidenceInterval(t);
 		predictionInterval(t);
+		printf("Would you like to test hypotheses?\n");
+		printf("yes or no: "); 
+		scanf("%s",string);
+		while(strcmp (string,"yes") == 0){
 		
-		printf("Please enter a claimed average for hypothesis testing:");
-		scanf("%lf",&hypothesis);
+			printf("Please enter a claimed average for hypothesis testing:");
+			scanf("%lf",&hypothesis);
+			printf("\nWhat sort of hypothesis test would you like to use?\n");
+			printf("Please type a,b,c for the type of hypothesis test:\n\n");
+			printf("  a)  Two-sided alternative test\n");
+			printf("  b)  One-sided alternative test: ");
+			printf("sample mean is not greater than hypothesis\n");
+			printf("  c)  One-sided alternative test: ");
+			printf("sample mean is not less than hypothesis\n\n");
 		
-		printf("\nWhat sort of hypothesis test would you like to use?\n");
-		printf("Please type a,b,c for the type of hypothesis test:\n\n");
-		printf("  a)  Two-sided alternative test\n");
-		printf("  b)  One-sided alternative test: ");
-		printf("sample mean is not greater than hypothesis\n");
-		printf("  c)  One-sided alternative test: ");
-		printf("sample mean is not less than hypothesis\n\n");
+			printf("Hypothesis test: ");
+			scanf(" %c",&hypothesisTestType);
 		
-		printf("Hypothesis test: ");
-		scanf(" %c",&hypothesisTestType);
+			printf("\n");
 		
-		printf("\n");
-		
-		if(hypothesisTesting(t,hypothesisTestType,hypothesis)==TRUE){
-			printf("The hypothesis should be rejected\n");
-			printf(" at a significance level of 0.05.\n");
-			if(hypothesisTestType=='a'){
-				printf("Using a two-sided alternative test.\n");
+			if(hypothesisTesting(t,hypothesisTestType,hypothesis)==TRUE){
+				printf("The hypothesis should be rejected\n");
+				printf(" at a significance level of 0.05.\n");
+				if(hypothesisTestType=='a'){
+					printf("Using a two-sided alternative test.\n");
+				} else {
+					printf("Using a one-sided alternative test.\n");
+				}
 			} else {
-				printf("Using a one-sided alternative test.\n");
+				printf("The hypothesis should not be rejected");
+				printf(" at a significance level of 0.05.\n");
+				if(hypothesisTestType=='a'){
+					printf("Using a two-sided alternative test.\n");
+				} else {
+					printf("Using a one-sided alternative test.\n");
+				}
 			}
-		} else {
-			printf("The hypothesis should not be rejected");
-			printf(" at a significance level of 0.05.\n");
-			if(hypothesisTestType=='a'){
-				printf("Using a two-sided alternative test.\n");
-			} else {
-				printf("Using a one-sided alternative test.\n");
-			}
-		}
-		
-		
-		
-		
+			printf("\nWould you like to test hypotheses?\n");
+			printf("type yes or no: "); 
+			scanf("%s",string);
+		}	
 	}
 	
 	
